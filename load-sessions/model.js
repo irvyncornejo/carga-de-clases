81d8416e2d5ecb70f45f0775df8bd1518c218bba
Irvyn Cornejo <irvyncornejo@gmail.com>
2021-07-17 00:27:15 -0500

    Se integró la opción para poder añadir contenido según la metología y definir si es premium o free la sesión

--- load-sessions/model.js
+++ load-sessions/model.js
@@ -14,56 +14,64 @@ class Session{
     return sheetData
   }
   getSession(){
-    const data = this.validate()
-    const lastRow = data.getLastRow()
-    const grade = data.getRange('F3').getValue().replace('°', '')
-    const section = this.getSection(grade)
-    const dataSession = {
-      course: data.getRange('C2').getValue(),
-      teachers: data.getRange('C3:C4').getValues().map(teacher => teacher[0]),
-      session: data.getRange('C5').getValue(),
-      period: data.getRange('F2').getValue(),
-      grade: Object.keys(section).includes('area') ? `${grade}${section.area.substring(1)}` : grade,
-      hours: data.getRange('F5').getValue(),
-      title: data.getRange('C6').getValue(),
-      purposes: data.getRange('C8').getValue(),
-      type: this.numberSession,
-      order: data.getRange(`B17:B${lastRow}`).getValues().map(ind => ind[0].toLowerCase()),
-      section: section.section,
-      previousKnowledge: data.getRange('H4').getValue()
-    }
-    dataSession['content'] = this.processLinks(data.getRange(`B17:C${lastRow}`).getValues())
-    return dataSession
+      const data = this.validate()
+      const lastRow = data.getLastRow()
+      const grade = data.getRange('F3').getValue().replace('°', '')
+      const section = this.getSection(grade)
+      const method = data.getRange('C10').getValue()
+      const content = data.getRange(`B17:C${lastRow}`).getValues()
+      const dataSession = {
+        course: data.getRange('C2').getValue(),
+        teachers: data.getRange('C3:C4').getValues().map(teacher => teacher[0]),
+        session: data.getRange('C5').getValue(),
+        period: data.getRange('F2').getValue(),
+        grade: Object.keys(section).includes('area') ? `${grade}${section.area.substring(1)}` : grade,
+        hours: data.getRange('F5').getValue(),
+        title: data.getRange('C6').getValue(),
+        purposes: data.getRange('C8').getValue(),
+        type: this.numberSession,
+        order: data.getRange(`B17:B${lastRow}`).getValues().map((content,index) => `${index}|${content[0].toLowerCase()}`),
+        section: section.section,
+        subscription: data.getRange('F10').getValue() === 'Free' ? 'free' : 'premium',
+        previousKnowledge: data.getRange('H4').getValue(),
+        method: method
+      }
+      dataSession['content'] = this.processContent(content, method)
+      return dataSession
   }
 
-  processLinks(content){
+  processContent(content, method){
     const contentSession = {}
-    content.forEach(value =>{
-      if(value[0] === 'Diagrama|Imagen' || value[0] === 'Vídeo'){
-        const links = this.trasnformLinksImg(value[1])
-        contentSession[`${value[0].toLowerCase()}`] = links
+    content.forEach((value, index) =>{
+      if((value[0] === 'Diagrama|Imagen' || value[0] === 'Vídeo') && method === 'Clásica'){
+        const links = this.trasnformLinksMedia(value[1])
+        contentSession[`${index}|${value[0].toLowerCase()}`] = links
+      }
+      else if(value[0] === 'Vídeo' && method === 'First Vídeo'){
+        const links = this.trasnformLinksMedia(value[1])
+        contentSession[`${index}|${value[0].toLowerCase()}`] = links
       }
       else if(value[0] === 'Documento'){
-        contentSession[`${value[0].toLowerCase()}`] = this.splitLinks(value[1])
+        contentSession[`${index}|${value[0].toLowerCase()}`] = this.splitLinks(value[1])
                                                           .map(url => `${url}?embedded=true`)
       }
       else if(value[0] === 'Link' || value[0] === 'Presentación'){
-        contentSession[`${value[0].toLowerCase()}`] = this.splitLinks(value[1])
+        contentSession[`${index}|${value[0].toLowerCase()}`] = this.splitLinks(value[1])
       }
       else{
-        contentSession[`${value[0].toLowerCase()}`] = value[1]
+        contentSession[`${index}|${value[0].toLowerCase()}`] = value[1]
       }
     })
     return contentSession
   }
-  
+
   getId(link, limit){
     const index = ['d/', limit].map(ind => link.indexOf(ind))
     const id = link.substring(index[0]+2, index[1])
     return id
   }
 
-  trasnformLinksImg(links){
+  trasnformLinksMedia(links){
     const linksArray = this.splitLinks(links)
     const linksT = []
     linksArray.forEach(link =>{
